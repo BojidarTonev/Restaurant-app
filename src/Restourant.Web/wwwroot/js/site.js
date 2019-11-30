761247d4b6012906a0f03016f7e9e2e35b89a778
Bojidar Tonev <ghost1993ifyy@gmail.com>
2019-11-30 23:46:15 +0200

    Added quantity for front end before order applied. Added js to increment or decrement quantity before submitting the order.

--- src/Restourant.Web/wwwroot/js/site.js
+++ src/Restourant.Web/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function attachMinusAndPlusEvents() {
+    document.querySelectorAll('#minus').forEach(item => {
+        item.addEventListener('click', event => {
+            let currentValue = item.parentNode.getElementsByClassName('menu-order-number')[0].innerText;
+            let result = Number(currentValue) - 1;
+
+            if (result >= 0) {
+                item.parentNode.getElementsByClassName('menu-order-number')[0].innerText = result;
+            }
+        })
+    })
+    document.querySelectorAll('#plus').forEach(item => {
+        item.addEventListener('click', event => {
+            let currentValue = item.parentNode.getElementsByClassName('menu-order-number')[0].innerText;
+            let result = Number(currentValue) + 1;
+
+            item.parentNode.getElementsByClassName('menu-order-number')[0].innerText = result;
+        })
+    })
+
+} 
+
+attachMinusAndPlusEvents();
